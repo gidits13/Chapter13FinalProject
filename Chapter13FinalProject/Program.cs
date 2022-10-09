3699786488b67cb841e8e93142cbac9a7ed9e303
gidits13 <gidits13@gmail.com>
2022-10-09 11:57:56 +0300

    Добавьте файлы проекта.

---
+++ Chapter13FinalProject/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Chapter13FinalProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        string path= @"c:\2\Text1.txt";
+        string text="";
+        if (File.Exists(path))
+        {
+           using (StreamReader sr = new StreamReader(path))
+                {
+                    text = sr.ReadToEnd();
+                }
+         }
+         char[] split = new char[] { ' ', '\r', '\n' };
+         var array = text.Split(split, StringSplitOptions.RemoveEmptyEntries);
+
+            var stopWatch1 = new Stopwatch();
+            var stopWatch2 = new Stopwatch();
+
+            stopWatch1.Start();
+            AddToList(array);
+            stopWatch1.Stop();
+            Console.WriteLine("Время выполнения AddToList:");
+            Console.WriteLine(stopWatch1.Elapsed);
+
+            stopWatch2.Start();
+            AddToLinkedList(array);
+            stopWatch2  .Stop();
+            Console.WriteLine("Время выполнения AddToLinkedList:");
+            Console.WriteLine(stopWatch2.Elapsed);
+        }
+        static void AddToList(string[] words)
+        {
+            List<string> list = new List<string>();
+            foreach(var w in words)
+                list.Add(w.ToString()); 
+        }
+        static void AddToLinkedList(string[] Words)
+        {
+            LinkedList<string> list = new LinkedList<string>(); 
+            foreach (var W in Words)    
+                list.AddLast(W.ToString()); 
+        }
+    }
+}
---
+++ Chapter13FinalProjectTask2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+
+namespace Chapter13FinalProjectTask2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\2\Text1.txt";
+            string text = "";
+            char[] split = new char[] { ' ', '\r', '\n' };
+            if (File.Exists(path))
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    text = sr.ReadToEnd();
+                }
+            }
+            var noPunctuationText = new string(text.Where(c => !char.IsPunctuation(c)).ToArray());
+            var array = noPunctuationText.Split(split,StringSplitOptions.RemoveEmptyEntries);
+            TopTenWords(array);
+
+            
+        }
+        static void TopTenWords(string[] words)
+        {
+            Dictionary<string, int> wordCount = new Dictionary<string, int>();
+            foreach(var word in words)
+            {
+                if(!wordCount.ContainsKey(word))
+                {
+                    wordCount[word]=1;
+                }
+                else
+                {
+                    wordCount[word]++;
+                }
+
+            }
+
+            //wordCount.OrderBy(x => x.Value);
+            wordCount = wordCount.OrderByDescending(x=>x.Value).ToDictionary(x=>x.Key,x=>x.Value);
+            var ten = wordCount.Take(10).Select(c=>c.Key).ToList();   
+            foreach(var t in ten)
+                Console.WriteLine(t);
+        }
+    }
+}
